3a1ec71386eace13b391f3ae2a279598c77d5ab6
Alex <alexandersychev2005@gmail.com>
2024-04-27 14:59:50 +0300

    Updated allowed hosts

--- dictionaryWeb/settings.py
+++ dictionaryWeb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 LOGIN_URL = 'login/'
 # Application definition
